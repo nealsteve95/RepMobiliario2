d2c025fcf4761d6f9c9e8d40600264a0d7955fbd
Neals Paye <neals.paye@tecsup.edu.pe>
2023-05-18 10:02:26 -0500

    cambio de conexion

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,12 +1,12 @@
 <?php 
-$contrasena = "";
-$usuario = "root";
-$nombre_bd = "inmobiliaria";
+$contrasena = "AVNS_JoHKvJ-siwLjSK225X1";
+$usuario = "doadmin";
+$nombre_bd = "defaultdb";
 
 try {
 	$bd = new PDO (
-		'mysql:host=localhost;
-		dbname='.$nombre_bd,
+		'db-mysql-nyc1-15835-do-user-14090458-0.b.db.ondigitalocean.com'
+		.$nombre_bd,
 		$usuario,
 		$contrasena,
 		array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
