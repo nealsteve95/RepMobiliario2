4823bf66744609413750852b0849973a3732fa12
Neals Paye <neals.paye@tecsup.edu.pe>
2023-05-18 10:24:04 -0500

    Cambio 2 en la base de datos

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -2,13 +2,14 @@
 $contrasena = "AVNS_JoHKvJ-siwLjSK225X1";
 $usuario = "doadmin";
 $nombre_bd = "defaultdb";
+$host = "db-mysql-nyc1-15835-do-user-14090458-0.b.db.ondigitalocean.com";
+$port = "25060";
 
 try {
 	$bd = new PDO (
-		'db-mysql-nyc1-15835-do-user-14090458-0.b.db.ondigitalocean.com'
-		.$nombre_bd,
-		$usuario,
-		$contrasena,
+		'mysql:host='.$host.';port='.$port.';dbname='.$nombre_bd,
+        $usuario,
+        $contrasena,
 		array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
 	);
 } catch (Exception $e) {
